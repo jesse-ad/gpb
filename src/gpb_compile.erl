4f821ddb1a3183454e8f9b7700a8de1d90299b9e
jesse-ad <jesse@adroll.com>
2012-03-12 16:18:53 -0700

    Backed out experimental change around 'repeated' fields.  This is a limitation we'll have to live with for now

--- src/gpb_compile.erl
+++ src/gpb_compile.erl
@@ -1008,10 +1008,10 @@ format_msg_encoder(MsgName, MsgDef) ->
                        RIndent, "", FieldEncoderFn, FName, I-1, KeyTxt,
                        RIndent, ""]);
                Occurrence == repeated ->
-                    f("    ~sif F~s == [] orelse (F~s == undefined) -> Bin~w;~n"
+                    f("    ~sif F~s == [] -> Bin~w;~n"
                       "    ~*s  true -> ~p(F~s, Bin~w)~n"
                       "    ~*send",
-                      [ResultVar, FName, FName, I-1,
+                      [ResultVar, FName, I-1,
                        RIndent, "", FieldEncoderFn, FName, I-1,
                        RIndent, ""]);
                Occurrence == required ->
